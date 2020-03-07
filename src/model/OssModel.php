583c51402ed3d3057d87e825ad4e019646a23233
eson <834767372@qq.com>
2020-03-08 02:38:52 +0800

    【更新】- 用缓存redis方式存储失效前的oss链接

--- src/model/OssModel.php
+++ src/model/OssModel.php
@@ -8,6 +8,7 @@
 
 namespace model;
 
+use Redis;
 
 /**
  * @desc 阿里云对象云存储 Class OssModel
@@ -16,11 +17,20 @@ namespace model;
 class OssModel
 {
     /*配置属性*/
-    public $_config = [];
+    private $_config = [];
+    /*缓存redis方式*/
+    public $redis = null;
+    /*缓存时间*/
+    public $cacheTime = 3600;
 
     public function __construct ()
     {
         $this->_config = config('oss.params');
+
+        $this->redis = new Redis();
+        $this->redis->connect('127.0.0.1', 6379);
+        $this->redis->auth('');
+        $this->redis->select(1);
     }
 
     /**
@@ -30,8 +40,15 @@ class OssModel
      */
     public function goToUrl ($file)
     {
+        // 检查redis是否有键值
+        $url = $this->getCache($file);
+        if ($url) {
+            Header("Location:$url");
+            return true;
+        }
+
         $bucket_name = $this->_config['bucket'];
-        $expire = time() + 3600;
+        $expire = time() + $this->cacheTime;
         $StringToSign = "GET\n\n\n" . $expire . "\n/" . $bucket_name . "/" . $file;
         $Sign = base64_encode(
             hash_hmac(
@@ -45,7 +62,20 @@ class OssModel
         $path = urlencode($file);
         $url = "{$this->_config['endpoint']}{$path}?OSSAccessKeyId={$this->_config['accessKeyId']}&&Expires={$expire}&Signature={$sign_url_encode}";
 
+        // 存储redis
+        $this->setCache($file, $url);
+
         Header("Location:$url");
         return true;
+    }
+
+    public function getCache ($key)
+    {
+        return $this->redis->get($key);
+    }
+
+    public function setCache ($key, $val)
+    {
+        return $this->redis->set($key, $val, $this->cacheTime);
     }
 }
