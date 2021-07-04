85e4b29adb9825531179872d7c4e062e0b3bbbfb
eson <834767372@qq.com>
2021-07-04 13:55:17 +0800

    【更新】- 搜索结果刷新页面不重置

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -13,7 +13,7 @@ document.querySelector('h1').after(form);
 let listItems = [].slice.call(document.querySelectorAll('.movie-list li'));
 
 input.addEventListener('keyup', function () {
-    var i,
+    let i,
         // e = "^(?=.*\\b" + this.value.trim().split(/\s+/).join("\\b)(?=.*\\b") + ").*$",
         e = this.value.trim().split(/\s+/).join("\\b)(?=.*\\b"),
         n = RegExp(e, "i");
@@ -27,3 +27,31 @@ input.addEventListener('keyup', function () {
         item.hidden = true;
     });
 });
+
+if (getQueryVariable('filter')) {
+    input.value = getQueryVariable('filter');
+    let i,
+        // e = "^(?=.*\\b" + this.value.trim().split(/\s+/).join("\\b)(?=.*\\b") + ").*$",
+        e = input.value.trim().split(/\s+/).join("\\b)(?=.*\\b"),
+        n = RegExp(e, "i");
+    listItems.forEach(function(item) {
+        item.removeAttribute('hidden');
+    });
+    listItems.filter(function(item) {
+        i = item.querySelector('p').textContent.replace(/\s+/g, " ");
+        return !n.test(i);
+    }).forEach(function(item) {
+        item.hidden = true;
+    });
+}
+
+function getQueryVariable(variable)
+{
+    let query = window.location.search.substring(1);
+    let vars = query.split("&");
+    for (let i=0;i<vars.length;i++) {
+        let pair = vars[i].split("=");
+        if(pair[0] === variable){return decodeURI(pair[1]);}
+    }
+    return(false);
+}
